36fd8644e46cc4a7bd83e7f073f21e5383e21390
Marcus Vinicius <marcuslops.au@gmail.comgit congif user.emailgit config --global user.name Marcus>
2022-04-04 17:57:53 +0930

    New commit

--- assets/script.js
+++ assets/script.js
@@ -1,105 +1,114 @@
-var buttonEl = document.querySelector('#button');
-var iniciarQuiz = document.querySelector('.iniciarQuiz');
-var fistQuestion = document.querySelector('.fistQuestion');
-var secondQuestion = document.querySelector('.secondQuestion');
-var thirdQuestion = document.querySelector('.thirdQuestion');
-var fourthQuestion = document.querySelector('.fourthQuestion');
-var pontuação = document.querySelector('.pontuação');
-var timeEl = document.querySelector('.timer');
-//fistQuestion
-var goku = document.querySelector('#goku');
-var vegeta = document.querySelector('#vegeta');
-var gohan = document.querySelector('#gohan');
-var broly = document.querySelector('#broly');
-//secondQuestion
-var bra = document.querySelector('#bra');
-var rio = document.querySelector('#rio');
-var sp = document.querySelector('#sp');
-var belo = document.querySelector('#belo');
-//thirthQuestion
-var n7 = document.querySelector('#n7');
-var n10 = document.querySelector('#n10');
-var n4 = document.querySelector('#n4');
-var n9 = document.querySelector('#n9');
-//fourthQuestion
-var champa = document.querySelector('#champa');
-var bills = document.querySelector('#bills');
+var buttonStartQuiz = document.querySelector('#button');
+var startQuiz = document.querySelector('.iniciarQuiz');
+var questionsDiv = document.querySelector('.questions');
+var answersButtons = document.querySelector('#buttons');
+var points = document.querySelector('.points');
+var myTimer = document.querySelector('#timer')
+//Question
+var question = document.querySelector('#question');
+var ans1 = document.querySelector('#ans1');
+var ans2 = document.querySelector('#ans2');
+var ans3 = document.querySelector('#ans3');
 
+var questions = [
+    {
+        question: 'Which of these characters is the protagonist of Dragon Ball?',
+        options: [
+            'Gohan',
+            'Goku',
+            'vegeta',
+        ],
+        correct: 1
+    },
+    {
+        question: 'How many Saiyans are still alive and living on Earth?',
+        options: [
+            '10 saiyans',
+            '7 saiyans',
+            '6 saiyans',
+        ],
+        correct: 1
+    },
+    {
+        question: "What is the name of Universe 7's God of Destruction ?",
+        options: [
+            'Beerus',
+            'Champa',
+            'Sidra',
+        ],
+        correct: 0
+    },
+    {
+        question: 'How many participants does the Tournament of Power have?',
+        options: [
+            '100 warriors',
+            '60 warriors',
+            '80 warriors',
+        ],
+        correct: 2
+    },
+    {
+        question: 'How many universes are there in dragon ball?',
+        options: [
+            '7 universes',
+            '10 universes',
+            '12 universes',
+        ],
+        correct: 2
+    }
+];
+
+var currentQuestion = 0;
 
 //botão para começar o quiz e iniciar a primeira pergunta
-buttonEl.addEventListener('click', function (){  
-    iniciarQuiz.setAttribute('style', 'display: none;');
-
-    fistQuestion.setAttribute('style', 'display: block;');
-    timeEl.setAttribute('style', 'display: block;');
+buttonStartQuiz.addEventListener('click', function (){  
+    hideStartQuiz();
+    showQuestionDiv();
+    showNextQuestion();
+    startTimer();  
 });
 
+answersButtons.addEventListener('click', function(event){
+    var clickedBtn = event.target;
+    console.log(clickedBtn);
+    if(clickedBtn.matches('button')){
+        var optionIndex = clickedBtn.getAttribute('data-index');
+        console.log(optionIndex);
+        console.log(currentQuestion)
+    }
+    if(currentQuestion < questions.length-1){
+        currentQuestion++;
+        showNextQuestion();
+    }else{
+        console.log('implement end game')
+    }
+})
 
-function countdown(){
+function showQuestionDiv(){
+    questionsDiv.setAttribute('style', 'display: block;');
+}
+
+function hideStartQuiz(){
+    startQuiz.setAttribute('style', 'display: none;');
+}
+
+function showNextQuestion(){
+    question.textContent = questions[currentQuestion].question;
+    ans1.textContent = questions[currentQuestion].options[0];
+    ans2.textContent = questions[currentQuestion].options[1];
+    ans3.textContent = questions[currentQuestion].options[2];   
+}
+
+function startTimer(){
     var timer = 60;
-
     var timeInterval = setInterval(function(){
         timer--;
-        timerEl.textContent = timer + "seconds left";
-        
-        if(timer === 0){
-            clearInterval(timeInterval);           
-        }
+        myTimer.textContent = timer;
 
+        if(timer <= 0){
+            clearInterval(timeInterval);
+        }
     }, 1000);
 }
 
-goku.addEventListener('click', function (){
-    alert('certo'); 
-    fistQuestion.setAttribute('style', 'display: none;');
-    secondQuestion.setAttribute('style', 'display: block;');  
-});
-
-// vegeta.addEventListener('click', function (){
-//     alert('errado')
-// })
-// gohan.addEventListener('click', function (){
-//     alert('errado')
-// })
-// broly.addEventListener('click', function (){
-//     alert('errado')
-// })
-
-bra.addEventListener('click', function (){
-    alert('certo');
-    secondQuestion.setAttribute('style', 'display: none;');
-    thirdQuestion.setAttribute('style', 'display: block;');
-});
-
-// belo.addEventListener('click', function (){
-//     alert('errado')
-// });
-// rio.addEventListener('click', function (){
-//     alert('errado')
-// });
-// sp.addEventListener('click', function (){
-//     alert('errado')
-// });
-
-n7.addEventListener('click', function (){
-    alert('certo');
-    thirdQuestion.setAttribute('style', 'display: none;');
-    fourthQuestion.setAttribute('style', 'display: block;');
-});
-
-// n10.addEventListener('click', function (){
-//     alert('errado')
-// });
-// n4.addEventListener('click', function (){
-//     alert('errado')
-// });
-// n6.addEventListener('click', function (){
-//     alert('errado')
-// });
-
-bills.addEventListener('click', function (){
-    alert('certo');
-    fourthQuestion.setAttribute('style', 'display: none;');
-    pontuação.setAttribute('style', 'display: block;');
-});
 
