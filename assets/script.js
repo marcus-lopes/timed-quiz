2aea9ae64e5e80871e7c2b2b7c9e85edd70c9c86
Marcus Vinicius <marcuslops.au@gmail.comgit congif user.emailgit config --global user.name Marcus>
2022-04-06 15:59:42 +0930

    correction

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,15 @@ var startQuiz = document.querySelector('.startQuiz');
 var questionsDiv = document.querySelector('.questions');
 var answersButtons = document.querySelector('#buttons');
 var points = document.querySelector('.points');
-var myTimer = document.querySelector('#timer')
+var myTimer = document.querySelector('#timer');
+var youLoser = document.querySelector('.youLoser');
 //Question
 var question = document.querySelector('#question');
 var ans1 = document.querySelector('#ans1');
 var ans2 = document.querySelector('#ans2');
 var ans3 = document.querySelector('#ans3');
+// var ans4 = document.querySelector('#ans4');
+// var ans5 = document.querySelector('#ans5');
 var seconds = 60;
 var timeInterval ;
 var refresh ;
@@ -61,6 +64,8 @@ var questions = [
     }
 ];
 
+var questionIndex = 0;
+
 var lastResult = JSON.parse(localStorage.getItem("obj"));
 
 if(lastResult){
@@ -69,13 +74,10 @@ if(lastResult){
 
 function showLastResult(){
     var resultEl  = document.createElement('p');
-    resultEl.textContent = lastResult.name + ' got a score of: ' + lastResult.points + '/ with time remaining ' + lastResult.seconds;
+    resultEl.textContent = lastResult.name + ' got a score of: ' + lastResult.seconds;
     points.append(resultEl);
 }
 
-var currentQuestion = 0;
-
-//botão para começar o quiz e iniciar a primeira pergunta
 buttonStartQuiz.addEventListener('click', function (){  
     hideStartQuiz();
     showQuestionDiv();
@@ -88,15 +90,18 @@ answersButtons.addEventListener('click', function(event){
     console.log(clickedBtn);
     if(clickedBtn.matches('button')){
         var optionIndex = clickedBtn.getAttribute('data-index');
-        console.log(optionIndex);
-        console.log(currentQuestion);
-    }
-    if(currentQuestion < questions.length-1){
-        currentQuestion++;
-        showNextQuestion();
-    }else{
-        clearInterval(timeInterval);
-        endGame();
+
+        if(questions[questionIndex].correct != optionIndex ){
+            seconds = seconds-5;
+        }
+
+        if(questionIndex < questions.length-1){
+            questionIndex++;
+            showNextQuestion();
+        }else{
+            clearInterval(timeInterval);
+            endGame();
+        }
     }
 })
 
@@ -109,10 +114,10 @@ function hideStartQuiz(){
 }
 
 function showNextQuestion(){
-    question.textContent = questions[currentQuestion].question;
-    ans1.textContent = questions[currentQuestion].options[0];
-    ans2.textContent = questions[currentQuestion].options[1];
-    ans3.textContent = questions[currentQuestion].options[2];   
+    question.textContent = questions[questionIndex].question;
+    ans1.textContent = questions[questionIndex].options[0];
+    ans2.textContent = questions[questionIndex].options[1];
+    ans3.textContent = questions[questionIndex].options[2];   
 }
 
 function startTimer(){
@@ -123,15 +128,12 @@ function startTimer(){
         if(seconds === 0){
             clearInterval(timeInterval);
             hideStartQuiz(questionsDiv);
-            endGame();
-            // loser();
+            loser();
         }
     }, 1000);
 }
 
-function Punctuation(){
-     
-}
+
 
 function endGame(){
     var name = prompt("What's your name?");
@@ -145,10 +147,8 @@ function reload(){
 }
 
 // function loser(){
-//     var youLoser = "";
 //     if(timer === 0){ 
 //     youLoser.textContent = "You lost!!";
-//     console.log(youLoser);
 //  }
 // }
 
